38ca1dfb6465471886b7e68cd070f736fa2628e1
HarisAli <harisali03@proton.me>
2023-09-30 21:20:16 +0500

    Added Stack File

---
+++ Practice/Queue.cpp
@@ -0,0 +1,10 @@
+//
+// Created by haris on 9/30/2023.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+}
--- Practice/Stack.cpp
+++ Practice/Stack.cpp
@@ -1,3 +1,27 @@
 //
 // Created by haris on 9/28/2023.
 //
+
+#include <iostream>
+using namespace std;
+
+int STACK[10], top = -1;
+
+void Display(){
+    if (top == -1){
+        cout << "Stack OverFlow" << endl;
+    } else {
+        for (int i = top; i > ; i++){
+            cout << STACK
+        }
+    }
+}
+int main(){
+    ++top;
+    STACK[top] = 10;
+    if (top == - 1){
+        cout << "Stack OverFlow" << endl;
+    }
+
+}
+
