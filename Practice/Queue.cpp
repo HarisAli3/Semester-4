0894324c2d5645ac3acca81d08b3662a8c803d4c
HarisAli <harisali03@proton.me>
2023-10-01 00:15:33 +0500

    Updated Queue Algorithm

--- Practice/Queue.cpp
+++ Practice/Queue.cpp
@@ -5,22 +5,21 @@
 #include <iostream>
 using namespace std;
 
-const int n = 10;
+const int n = 5;
 int First = -1;
 int Rear = -1;
 int Array[n];
 
-void Insert(){
-    if (Rear >= n - 1){
-        cout << "Queue OverFlow" << endl;
-    } else {
-        Rear++;
-        Array[Rear] = 10;
+void Insert(int X){
+        if (Rear > n - 1){
+            cout << "Queue OverFlow" << endl;
+        } else {
+            Array[++Rear] = X;
     }
 }
 
 void Delete(){
-    if (First && Rear){
+    if (First == Rear){
         cout << "Queue UnderFlow" << endl;
     } else {
         First++;
@@ -30,8 +29,22 @@ void Display(){
     for(int i = ++First; i <= Rear; i++){
         cout << Array[i] << endl;
     }
+    --First;
 }
 int main(){
-    Insert();
+    Insert(10);
+    Display();
+    Insert(20);
+    Display();
+    Insert(30);
+    Display();
+    Insert(40);
+    Display();
+    Insert(50);
+    Display();
+    Insert(60);
+    Display();
+    Delete();
+    cout << "End" << endl;
     Display();
 }
