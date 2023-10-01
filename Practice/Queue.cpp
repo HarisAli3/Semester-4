e2afe1fd81e6b49dd6b6b817b2a25621c4d2e324
HarisAli <harisali03@proton.me>
2023-10-01 23:17:26 +0500

    Updated Insertion Function

--- Practice/Queue.cpp
+++ Practice/Queue.cpp
@@ -1,23 +1,27 @@
 //
 // Created by haris on 9/30/2023.
-
+//
 
 #include <iostream>
 using namespace std;
 
-void insertion(int Array[], int n, int &First, int &Rear) {
-    int X;
-    cout << "Enter Value to Enter";
-    cin >> X;
-
-    if (Rear >= n - 1){
-        cout << "Queue OverFlow" << endl;
-    } else {
-        Array[++Rear] = X;
+void insertion(int Array[], int n, int &Rear) {
+    int number;
+    cout << "How many Values you want to enter?";
+    cin >> number;
+    for (int i = 0 ; i < number; i++){
+        if (Rear >= n - 1){
+            cout << "Queue OverFlow" << endl;
+        } else {
+            int X;
+            cout << "Enter Value to Enter:";
+            cin >> X;
+            Array[++Rear] = X;
+        }
     }
 }
 
-void deletion(int Array[], int n, int &First, int &Rear) {
+void deletion(int &First, int &Rear) {
     if (First == Rear){
         cout << "Queue UnderFlow" << endl;
     } else {
@@ -25,7 +29,8 @@ void deletion(int Array[], int n, int &First, int &Rear) {
     }
 }
 
-void display(int Array[],int n, int &First, int &Rear) {
+void display(int Array[], int &First, int &Rear) {
+
     for (int i = ++First; i <= Rear; i++) {
         cout << Array[i] << endl;
     }
@@ -33,42 +38,50 @@ void display(int Array[],int n, int &First, int &Rear) {
 }
 
 int main(){
-    bool close = false;
-    int choice;
 
     int size;
     cout << "Enter Size for Queue:";
     cin >> size;
-
     int array[size];
+
     int First = -1;
     int Rear = -1;
 
+    bool close = false;
+    int choice;
+
     while(!close){
+
         cout << "Queue Operations:" << endl;
         cout << "1: En-Queue (Insertion)" << endl;
         cout << "2: De-Queue (Deletion)" << endl;
         cout << "3: Display" << endl;
         cout << "4: Show First and Rear Value" << endl;
         cout << "0: Close Program" << endl;
+        cout << "Choice:";
+
         cin >> choice;
 
-        switch (choice){
+        switch (choice) {
+
             case 1:
-                insertion(array, size, First, Rear);
+                insertion(array, size, Rear);
                 break;
             case 2:
-                deletion(array, size, First, Rear);
+                deletion(First, Rear);
                 break;
             case 3:
-                display(array, size, First, Rear);
+                display(array, First, Rear);
                 break;
             case 4:
-                cout << "First:" << First << endl;
-                cout << "Rear" << Rear << endl;
+                cout << "First: " << First << endl;
+                cout << "Rear: " << Rear << endl;
                 break;
             case 0:
                 close = true;
+                break;
+            default:
+                cout << "You Entered Invalid Option" << endl;
                 break;
 
         }
