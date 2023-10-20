53026a9163e345856c34f755b754cf588fbc77fe
HarisAli3 <70131147@student.uol.edu.pk>
2023-10-20 09:50:25 +0500

    Merge List Function

--- Lab Code/Lab 6/doubleLinkedLists.cpp
+++ Lab Code/Lab 6/doubleLinkedLists.cpp
@@ -17,30 +17,62 @@ void insert_end(Node **head, int newData){
     newNode->data = newData;
     newNode->prev = *head;
     newNode->next = nullptr;
+
     if (*head == nullptr){
         *head = newNode;
         newNode->prev = nullptr;
         return;
     }
     Node *temp = *head;
+
     while (temp->next != nullptr){
         temp = temp->next;
     }
+
     newNode->prev = temp;
     temp->next = newNode;
+
 }
 
 void display(Node **head){
     Node *list = *head;
+
     if (*head == nullptr){
         cout << "Empty List" << endl;
-    } else {
-        while (list){
-            cout << "Data: " << list->data << endl;
-            list = list->next;
-        }
+        return;
+    }
+    while (list != nullptr){
+        cout << "Data: " << list->data << endl;
+        list = list->next;
     }
 }
+
+void merge_list(Node **list1, Node **list2, Node **list3){
+
+    Node *temp1 = *list1;
+    Node *temp2 = *list2;
+
+    while (temp1 != nullptr && temp2 != nullptr){
+        if (temp1->data > temp2->data){
+            insert_end(list3, temp2->data);
+            temp2 = temp2->next;
+        } else {
+            insert_end(list3, temp1->data);
+            temp1 = temp1->next;
+        }
+    }
+
+    while(temp1 == nullptr && temp2 != nullptr){
+        insert_end(list3, temp2->data);
+        temp2 = temp2->next;
+    }
+
+    while(temp1 != nullptr && temp2 == nullptr){
+        insert_end(list3, temp1->data);
+        temp1 = temp1->next;
+    }
+}
+
 int main(){
 
     Node *list1 = nullptr;
@@ -48,6 +80,7 @@ int main(){
     insert_end(&list1, 2);
     insert_end(&list1, 5);
     insert_end(&list1, 8);
+    insert_end(&list1, 13);
     cout << "Double Linked List 1:" << endl;
     display(&list1);
 
@@ -55,8 +88,13 @@ int main(){
     insert_end(&list2, 1);
     insert_end(&list2, 6);
     insert_end(&list2, 10);
-    insert_end(&list2, 13);
+//    insert_end(&list2, 13);
     cout << "Double Linked List 2:" << endl;
     display(&list2);
+
+    Node *list3 = nullptr;
+    merge_list(&list1, &list2, &list3);
+    cout << "Double Linked List 3:" << endl;
+    display(&list3);
 
 }
